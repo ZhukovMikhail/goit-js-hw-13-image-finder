1131a475e3db718e532e0099adc1d6eb8f561fc9
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-08-20 00:36:51 +0300

    created dinamic list of images
    created dinamic list of images

--- src/apiService.js
+++ src/apiService.js
@@ -1,8 +1,9 @@
-export default function fetchImage(searhcQuerry, pageNumber, perPageNumber) {
+// export default function fetchImage(searhcQuerry, pageNumber, perPageNumber) {
+export default function fetchImage(searhcQuerry) {
   const USER_KEY = '22985243-b477986a48324befacd1d8a65';
   //   const pageNumber = null;
   //   const perPageNumber = 12;
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searhcQuerry}&page=${pageNumber}&per_page=${perPageNumber}&key=${USER_KEY}`;
+  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searhcQuerry}&page=1per_page=12&key=${USER_KEY}`;
   return fetch(url)
     .then(response => response.json())
     .catch(error => console.log(error));
--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import './sass/main.scss';
 import fetchImage from './apiService';
 
-import cardsListMarkup from './imageCardTmpl.hbs';
+import cardsListMarkup from './imageList.hbs';
+const basicLightbox = require('basiclightbox');
 // import cardMarkup from './imageCardTmpl.hbs';
+const instance = basicLightbox.create(`
+	<h1>Dynamic Content</h1>
+	<p>You can set the content of the lightbox with JS.</p>
+`);
 
 // fetchImage();
 const refs = {
   input: document.querySelector('.input'),
   submitBtn: document.querySelector('.submit_btn'),
   downloadMoreBtn: document.querySelector('.download-more_btn'),
-  renderBox: document.querySelector('.renderBpx'),
+  renderBox: document.querySelector('.renderBox'),
 };
-refs.submitBtn.addEventListener('submit', onSubmitBtn);
-const searhcQuerry = input.value.trim();
-let pageNumber = 0;
+refs.submitBtn.addEventListener('click', onSubmitBtn);
 
-refs.downloadMoreBtn.addEventListener('submit', onDownloadMoreBtn);
+// console.log();
+
+// let pageNumber = 0;
+
+refs.downloadMoreBtn.addEventListener('click', onDownloadMoreBtn);
 
 function onSubmitBtn(e) {
+  // console.log(refs.input.value);
   e.preventDefault();
-  fetchImage(searhcQuerry, pageNumber).then(data => {
-    refs.renderBox.innerHTML = cardsListMarkup(data);
+  const searhcQuerry = refs.input.value;
+  console.log(searhcQuerry);
+
+  fetchImage(searhcQuerry).then(data => {
+    refs.renderBox.innerHTML = cardsListMarkup(data.hits);
+    console.log(data.hits);
   });
 }
-
 function onDownloadMoreBtn() {
   e.preventDefault();
   pageNumber += 1;
