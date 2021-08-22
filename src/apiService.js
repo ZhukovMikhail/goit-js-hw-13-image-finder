2df01ba3809e07254ca4323e1074e51a4d88ac50
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-08-22 03:55:15 +0300

    added scrollIntoView()
    added scrollIntoView()

--- src/apiService.js
+++ src/apiService.js
@@ -1,10 +1,27 @@
 // export default function fetchImage(searhcQuerry, pageNumber, perPageNumber) {
-export default function fetchImage(searhcQuerry) {
-  const USER_KEY = '22985243-b477986a48324befacd1d8a65';
-  //   const pageNumber = null;
-  //   const perPageNumber = 12;
-  const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searhcQuerry}&page=1per_page=12&key=${USER_KEY}`;
-  return fetch(url)
-    .then(response => response.json())
-    .catch(error => console.log(error));
+export default class fetchImage {
+  constructor() {
+    this.searhcQuery = '';
+    this.pageNumber = 1;
+    this.perPage = 12;
+  }
+  fethArticles() {
+    const USER_KEY = '22985243-b477986a48324befacd1d8a65';
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searhcQuery}&page=${this.pageNumber}&per_page=${this.perPage}&key=${USER_KEY}`;
+    return fetch(url)
+      .then(response => response.json())
+      .catch(error => console.log(error));
+  }
+  get query() {
+    return this.searhcQuery;
+  }
+  set query(newQuery) {
+    this.searhcQuery = newQuery;
+  }
+  pageLim() {
+    this.perPage += 12;
+  }
+  page() {
+    this.pageNumber += 1;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,52 @@
 import './sass/main.scss';
-import fetchImage from './apiService';
-
+import fetchImageApi from './apiService';
 import cardsListMarkup from './imageList.hbs';
-
+const fetchImage = new fetchImageApi();
 // import cardMarkup from './imageCardTmpl.hbs';
-
 const refs = {
-  input: document.querySelector('.input'),
+  searchForm: document.getElementById('search-form'),
   submitBtn: document.querySelector('.submit_btn'),
   downloadMoreBtn: document.querySelector('.download-more_btn'),
   renderBox: document.querySelector('.renderBox'),
+  gallery: document.querySelector('.gallery'),
 };
-refs.submitBtn.addEventListener('click', onSubmitBtn);
-
-// console.log();
-
-// let pageNumber = 0;
-
+refs.searchForm.addEventListener('submit', onSubmitBtn);
 refs.downloadMoreBtn.addEventListener('click', onDownloadMoreBtn);
 
 function onSubmitBtn(e) {
-  // console.log(refs.input.value);
   e.preventDefault();
-  const searhcQuerry = refs.input.value;
-  console.log(searhcQuerry);
-
-  fetchImage(searhcQuerry).then(data => {
-    refs.renderBox.innerHTML = cardsListMarkup(data.hits);
-    console.log(data.hits);
+  fetchImage.query = e.currentTarget.elements.query.value;
+  fetchImage.perPage = 12;
+  this.pageNumber = 1;
+  // console.log(searhcQuery);
+  fetchImage.fethArticles().then(data => {
+    refs.gallery.innerHTML = cardsListMarkup(data.hits);
   });
 }
-function onDownloadMoreBtn() {
+function onDownloadMoreBtn(e) {
   e.preventDefault();
-  pageNumber += 1;
+
+  fetchImage.page();
+  fetchImage.fethArticles().then(data => {
+    refs.gallery.insertAdjacentHTML('beforeend', cardsListMarkup(data.hits));
+  });
+  refs.gallery.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
+import * as basicLightbox from 'basiclightbox';
+
+const instance = basicLightbox.create(
+  `
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`,
+);
+// refs.renderBox.addEventListener('click', onRenderClick);
+// functiononRenderClick
+instance.show();
